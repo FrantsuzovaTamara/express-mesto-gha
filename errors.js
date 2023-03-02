01160c48b8e09ae52ab994a474e567b6273e7302
Tamara Frantsuzova <t.f.66625262@yandex.ru>
2023-03-03 00:39:00 +0300

    fix: исправлена обработка ошибки при создании карточек и пользователей

--- errors.js
+++ errors.js
@@ -1,4 +1,4 @@
-module.exports.formatErrorMessage = (message, errors) => {
+const formatErrorMessage = (message, errors) => {
   const errorMessages = [];
   for (let i = 0; i < errors.length; i++) {
     errorMessages.push(message.split(`${errors[i]}: `)[1].split('.')[0]);
@@ -10,4 +10,4 @@ const VALIDATION_ERROR_CODE = 400;
 const NOT_FOUND_ERROR_CODE = 404;
 const OTHER_ERROR_CODE = 500;
 
-module.exports = { VALIDATION_ERROR_CODE, NOT_FOUND_ERROR_CODE, OTHER_ERROR_CODE };
+module.exports = { formatErrorMessage, VALIDATION_ERROR_CODE, NOT_FOUND_ERROR_CODE, OTHER_ERROR_CODE };
