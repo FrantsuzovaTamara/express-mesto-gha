1f3f6baf22712480004bfa0cf86e8083a761cf49
Tamara Frantsuzova <107715956+FrantsuzovaTamara@users.noreply.github.com>
2023-03-14 23:53:14 +0300

    Update cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -11,23 +11,23 @@ const {
 router.get('/', getCards);
 router.delete('/:_id', celebrate({
   body: Joi.object().keys({
-    _id: Joi.string().require().RegExp(/[a-z0-9]{24}/),
+    _id: Joi.string().required().RegExp(/[a-z0-9]{24}/),
   })
 }), deleteCard);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().require().min(2).max(30),
-    link: Joi.string().require().pattern(new RegExp('^https:\/\/+')),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(new RegExp('^https:\/\/+')),
   })
 }), createCard);
 router.put('/:_id/likes', celebrate({
   body: Joi.object().keys({
-    _id: Joi.string().require().RegExp(/[a-z0-9]{24}/),
+    _id: Joi.string().required().RegExp(/[a-z0-9]{24}/),
   })
 }), likeCard);
 router.delete('/:_id/likes', celebrate({
   body: Joi.object().keys({
-    _id: Joi.string().require().RegExp(/[a-z0-9]{24}/),
+    _id: Joi.string().required().RegExp(/[a-z0-9]{24}/),
   })
 }), dislikeCard);
 
