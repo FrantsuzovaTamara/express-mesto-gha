927e612ee411f6690b59f7ec35968126859e5770
Tamara Frantsuzova <107715956+FrantsuzovaTamara@users.noreply.github.com>
2023-03-15 00:12:08 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const {
   changeProfileInfo,
   changeAvatar,
 } = require('../controllers/users');
+const auth = require('./middlewares/auth');
 
 router.get('/me', getUser);
 router.patch('/me', celebrate({
@@ -14,15 +15,15 @@ router.patch('/me', celebrate({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
   })
-}), changeProfileInfo);
-router.patch('/me/avatar', celebrate({
+}), auth, changeProfileInfo);
+router.patch('/me/avatar', auth, celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().regex(/^http[s]{0,1}:\/\/[a-z0-9@!$&\'-._~:\/?#[\]@!$&()*+,;=]{5,}/).default(
       'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'
     ),
     })
-}), changeAvatar);
-router.get('/', getUsers);
-router.get('/:_id', getUserById);
+}), auth, changeAvatar);
+router.get('/', auth, getUsers);
+router.get('/:_id', auth, getUserById);
 
 module.exports = router;
